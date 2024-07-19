e851db8c10eadb636480969ef4f80f2ff63d7ae8
NextFire <git@yuru.moe>
2024-07-19 19:42:54 +0200

    [ai] ollama typing

--- nanachan/extensions/ai.py
+++ nanachan/extensions/ai.py
@@ -6,14 +6,13 @@ from dataclasses import dataclass, field
 
 import discord
 from discord import AllowedMentions, app_commands
-from ollama import Message
+from ollama import AsyncClient, Message
 
 from nanachan.discord.application_commands import LegacyCommandContext, legacy_command
 from nanachan.discord.bot import Bot
 from nanachan.discord.cog import NanaGroupCog
 from nanachan.discord.helpers import Embed, MultiplexingContext, MultiplexingMessage
-from nanachan.settings import RequiresAI
-from nanachan.utils.ollama import OllamaClient
+from nanachan.settings import OLLAMA_HOST, OLLAMA_MODEL, RequiresAI
 
 logger = logging.getLogger(__name__)
 
@@ -30,9 +29,16 @@ match_title = re.compile(r'"(.*)"')
 @RequiresAI
 @app_commands.guild_only()
 class AI(NanaGroupCog, group_name='ai'):
+    SYSTEM_PROMPT = """
+    You are a Discord bot for the Japan7 club.
+    You like Japanese culture, anime, music and games.
+    You are also knowledgeable about technical stuff, including programming and Linux.
+    Your replies are short and to the point.
+    """
+
     def __init__(self):
         super().__init__()
-        self.ollama = OllamaClient()
+        self.ollama = AsyncClient(host=OLLAMA_HOST)
         self.chats = defaultdict[int, ChatContext](ChatContext)
 
     @app_commands.command(name='chat')
@@ -60,7 +66,7 @@ class AI(NanaGroupCog, group_name='ai'):
             f'strictly adhering to the 100 characters limit and avoiding '
             f"the use of the word 'title': {content}"
         )
-        gen = await self.ollama.generate_typed(prompt, stream=False, system='')
+        gen = await self.ollama.generate(OLLAMA_MODEL, prompt)
         logger.info(f"generated title: {gen['response']}")
         resp_match = match_title.search(gen['response'])
         if resp_match is not None:
@@ -90,7 +96,7 @@ class AI(NanaGroupCog, group_name='ai'):
 
     async def chat_stream_blocks(self, messages: list[Message]):
         buf = ''
-        async for part in await self.ollama.chat_typed(messages, stream=True):
+        async for part in await self.ollama.chat(OLLAMA_MODEL, messages, stream=True):
             buf += part['message']['content']
             if len(buf) > 2000:
                 blocks = buf.split('\n')
