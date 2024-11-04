17c2bdaba573e27914e347a908f5ed38cf984b63
odrling <florianbadie@odrling.xyz>
2024-11-04 16:34:26 +0100

    [settings] improve typing of RequiredSettings
    shows that RequiredSettings can’t be used as a class decorator
    (the required_settings class parameter should be used instead)
    returning None instead of dummy functions seems more appropriate as then
    the caller can tell if the function is callable or not (but also we
    don’t use the decorator for anything now...)
    actually we don’t even use it.

--- nanachan/utils/settings.py
+++ nanachan/utils/settings.py
@@ -1,22 +1,18 @@
-import asyncio
-
-from nanachan.utils.misc import async_dummy, dummy
+from typing import Any, Callable
 
 __all__ = ('RequiredSettings',)
 
 
 class RequiredSettings:
 
-    def __init__(self, *args):
+    def __init__(self, *args: Any):
         self.configured = all(args)
 
     def __bool__(self):
         return self.configured
 
-    def __call__(self, func):
+    def __call__[T: Callable[..., Any]](self, func: T) -> T | None:
         if self.configured:
             return func
-        elif asyncio.iscoroutinefunction(func):
-            return async_dummy
         else:
-            return dummy
+            return None
