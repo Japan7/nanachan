54e60511c5559f1608b4d3e1e20d96f0694fd342
NextFire <git@yuru.moe>
2025-04-18 22:52:48 +0200

    [ai] helper class

--- nanachan/extensions/ai.py
+++ nanachan/extensions/ai.py
@@ -1,68 +1,29 @@
-import asyncio
 import logging
 from collections import defaultdict
-from dataclasses import dataclass
-from typing import Iterable, Sequence
 
 import discord
 from discord import AllowedMentions, app_commands
-from pydantic_ai import Agent, BinaryContent, RunContext, Tool
-from pydantic_ai.messages import (
-    FinalResultEvent,
-    FunctionToolCallEvent,
-    FunctionToolResultEvent,
-    ModelMessage,
-    PartDeltaEvent,
-    PartStartEvent,
-    TextPart,
-    TextPartDelta,
-    ToolCallPartDelta,
-    UserContent,
-)
+from pydantic_ai import BinaryContent
+from pydantic_ai.messages import ModelMessage, UserContent
 
 from nanachan.discord.application_commands import LegacyCommandContext, legacy_command
 from nanachan.discord.bot import Bot
 from nanachan.discord.cog import NanaGroupCog
 from nanachan.discord.helpers import Embed, MultiplexingContext, MultiplexingMessage, UserType
-from nanachan.nanapi.client import get_nanapi_tools
 from nanachan.settings import AI_MODEL, RequiresAI
+from nanachan.utils.ai import AgentHelper, RunDeps
 
 logger = logging.getLogger(__name__)
-
-
-@dataclass
-class RunDeps:
-    author: UserType
 
 
 @app_commands.guild_only()
 class AI(NanaGroupCog, group_name='ai', required_settings=RequiresAI):
     def __init__(self, bot: Bot):
-        super().__init__()
         self.bot = bot
-        self.agent = Agent(AI_MODEL, tools=list(self.get_tools()), deps_type=RunDeps)
+        assert AI_MODEL
+        self.model = AI_MODEL
+        self.agent = AgentHelper(self.model)
         self.messages = defaultdict[int, list[ModelMessage]](list)
-        self.lock = asyncio.Lock()
-
-    def get_tools(self) -> Iterable[Tool[RunDeps]]:
-        def get_current_user_infos(run_ctx: RunContext[RunDeps]):
-            """Get name and Discord ID of the current user."""
-            author = run_ctx.deps.author
-            return dict(
-                id=author.id,
-                display_name=author.display_name,
-                global_name=author.global_name,
-            )
-
-        yield Tool(get_current_user_infos)
-
-        def get_members_name_discord_id_map():
-            """Generate a mapping of Discord member display names to their Discord IDs."""
-            return {member.display_name: str(member.id) for member in self.bot.get_all_members()}
-
-        yield Tool(get_members_name_discord_id_map)
-
-        yield from get_nanapi_tools()
 
     @app_commands.command(name='chat')
     @legacy_command()
@@ -74,8 +35,7 @@ class AI(NanaGroupCog, group_name='ai', required_settings=RequiresAI):
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url)
         if image:
             embed.set_image(url=image.url)
-        assert AI_MODEL
-        embed.set_footer(text=AI_MODEL.model_name)
+        embed.set_footer(text=self.model.model_name)
         resp = await ctx.reply(embed=embed)
 
         attachments = [image] if image else []
@@ -99,7 +59,7 @@ class AI(NanaGroupCog, group_name='ai', required_settings=RequiresAI):
         attachments: list[discord.Attachment],
         reply_to: discord.Message | MultiplexingMessage | None = None,
     ):
-        async with thread.typing(), self.lock:
+        async with thread.typing():
             content: list[UserContent] = [prompt]
             for attachment in attachments:
                 if attachment.content_type and attachment.content_type.startswith('image/'):
@@ -108,73 +68,19 @@ class AI(NanaGroupCog, group_name='ai', required_settings=RequiresAI):
 
             history = self.messages[thread.id]
 
-            deps = RunDeps(author)
+            deps = RunDeps(self.bot, author)
 
             send = reply_to.reply if reply_to is not None else thread.send
             allowed_mentions = AllowedMentions.none()
             allowed_mentions.replied_user = True
 
             try:
-                async for block in self.agent_iter(content, history, deps):
+                async for block in self.agent.yield_agent_output(content, history, deps):
                     resp = await send(block, allowed_mentions=allowed_mentions)
                     send = resp.reply
             except Exception as e:
-                await send(f'An error occured while streaming the response:\n```\n{e}\n```')
+                await send(f'An error occured while streaming the response:\n{e}')
                 logger.exception(e)
-
-    async def agent_iter(
-        self,
-        user_prompt: Sequence[UserContent],
-        message_history: list[ModelMessage],
-        deps: RunDeps,
-    ):
-        """https://ai.pydantic.dev/agents/#streaming"""
-        async with self.agent.iter(user_prompt, message_history=message_history, deps=deps) as run:
-            async for node in run:
-                if Agent.is_user_prompt_node(node):
-                    # A user prompt node => The user has provided input
-                    ...
-                elif Agent.is_model_request_node(node):
-                    # A model request node => We can stream tokens from the model's request
-                    async with node.stream(run.ctx) as request_stream:
-                        buf = ''
-                        async for event in request_stream:
-                            if isinstance(event, PartStartEvent):
-                                if isinstance(event.part, TextPart):
-                                    buf += event.part.content
-                            elif isinstance(event, PartDeltaEvent):
-                                if isinstance(event.delta, TextPartDelta):
-                                    buf += event.delta.content_delta
-                                elif isinstance(event.delta, ToolCallPartDelta):
-                                    ...
-                            elif isinstance(event, FinalResultEvent):
-                                ...
-                            if len(buf) > 2000:
-                                blocks = buf.split('\n')
-                                buf = ''
-                                for block in blocks:
-                                    if len(buf) + len(block) > 2000:
-                                        yield buf
-                                        buf = block
-                                    else:
-                                        buf += '\n' + block
-                        if buf:
-                            yield buf
-                elif Agent.is_call_tools_node(node):
-                    # A handle-response node =>
-                    # The model returned some data, potentially calls a tool
-                    async with node.stream(run.ctx) as handle_stream:
-                        async for event in handle_stream:
-                            if isinstance(event, FunctionToolCallEvent):
-                                yield (
-                                    f'```\n[TOOL] {event.part.tool_name} {event.part.args}\n```'
-                                )
-                            elif isinstance(event, FunctionToolResultEvent):
-                                ...
-                elif Agent.is_end_node(node):
-                    # Once an End node is reached, the agent run is complete
-                    assert run.result
-                    message_history.extend(run.result.new_messages())
 
     @NanaGroupCog.listener()
     async def on_user_message(self, ctx: MultiplexingContext):
---
+++ nanachan/utils/ai.py
@@ -0,0 +1,144 @@
+import asyncio
+from dataclasses import dataclass
+from typing import AsyncGenerator, Iterable, Sequence
+
+from pydantic_ai import Agent, RunContext, Tool
+from pydantic_ai.messages import (
+    FinalResultEvent,
+    FunctionToolCallEvent,
+    FunctionToolResultEvent,
+    ModelMessage,
+    PartDeltaEvent,
+    PartStartEvent,
+    TextPart,
+    TextPartDelta,
+    ToolCallPartDelta,
+    UserContent,
+)
+from pydantic_ai.models import Model
+
+from nanachan.discord.bot import Bot
+from nanachan.discord.helpers import UserType
+from nanachan.nanapi.client import get_nanapi
+
+
+@dataclass
+class RunDeps:
+    bot: Bot
+    author: UserType
+
+
+class AgentHelper:
+    def __init__(self, model: Model):
+        self.agent = Agent(model, tools=list(tools()), deps_type=RunDeps)
+        self.lock = asyncio.Lock()
+
+    async def yield_agent_output(
+        self,
+        user_prompt: Sequence[UserContent],
+        message_history: list[ModelMessage],
+        deps: RunDeps,
+    ) -> AsyncGenerator[str]:
+        """https://ai.pydantic.dev/agents/#streaming"""
+        async with (
+            self.lock,
+            self.agent.iter(user_prompt, message_history=message_history, deps=deps) as run,
+        ):
+            async for node in run:
+                if Agent.is_user_prompt_node(node):
+                    # A user prompt node => The user has provided input
+                    ...
+                elif Agent.is_model_request_node(node):
+                    # A model request node => We can stream tokens from the model's request
+                    async with node.stream(run.ctx) as request_stream:
+                        buf = ''
+                        async for event in request_stream:
+                            if isinstance(event, PartStartEvent):
+                                if isinstance(event.part, TextPart):
+                                    buf += event.part.content
+                            elif isinstance(event, PartDeltaEvent):
+                                if isinstance(event.delta, TextPartDelta):
+                                    buf += event.delta.content_delta
+                                elif isinstance(event.delta, ToolCallPartDelta):
+                                    ...
+                            elif isinstance(event, FinalResultEvent):
+                                ...
+                            if len(buf) > 2000:
+                                blocks = buf.split('\n')
+                                buf = ''
+                                for block in blocks:
+                                    if len(buf) + len(block) > 2000:
+                                        yield buf
+                                        buf = block
+                                    else:
+                                        buf += '\n' + block
+                        if buf:
+                            yield buf
+                elif Agent.is_call_tools_node(node):
+                    # A handle-response node
+                    # => The model returned some data, potentially calls a tool
+                    async with node.stream(run.ctx) as handle_stream:
+                        async for event in handle_stream:
+                            if isinstance(event, FunctionToolCallEvent):
+                                yield f'`[TOOL] {event.part.tool_name} {event.part.args}`'
+                            elif isinstance(event, FunctionToolResultEvent):
+                                ...
+                elif Agent.is_end_node(node):
+                    # Once an End node is reached, the agent run is complete
+                    assert run.result
+                    message_history.extend(run.result.new_messages())
+
+
+def tools() -> Iterable[Tool[RunDeps]]:
+    yield Tool(get_current_user_infos)
+    yield Tool(get_members_name_discord_id_map)
+    yield from nanapi_tools()
+
+
+def get_current_user_infos(run_ctx: RunContext[RunDeps]):
+    """Get name and Discord ID of the current user."""
+    author = run_ctx.deps.author
+    return dict(id=author.id, display_name=author.display_name, global_name=author.global_name)
+
+
+def get_members_name_discord_id_map(run_ctx: RunContext[RunDeps]):
+    """Generate a mapping of Discord member display names to their Discord IDs."""
+    bot = run_ctx.deps.bot
+    return {member.display_name: member.id for member in bot.get_all_members()}
+
+
+def nanapi_tools():
+    nanapi = get_nanapi()
+    endpoints = [
+        nanapi.anilist.anilist_get_accounts,
+        nanapi.anilist.anilist_get_account_entries,
+        nanapi.anilist.anilist_get_medias,
+        nanapi.anilist.anilist_media_search,
+        nanapi.anilist.anilist_get_media_list_entries,
+        nanapi.anilist.anilist_get_media_chara_edges,
+        nanapi.anilist.anilist_get_charas,
+        nanapi.anilist.anilist_chara_search,
+        nanapi.anilist.anilist_get_chara_chara_edges,
+        nanapi.anilist.anilist_get_staffs,
+        nanapi.anilist.anilist_staff_search,
+        nanapi.anilist.anilist_get_staff_chara_edges,
+        nanapi.calendar.calendar_get_guild_events,
+        nanapi.histoire.histoire_histoire_index,
+        nanapi.histoire.histoire_get_histoire,
+        nanapi.projection.projection_get_projections,
+        nanapi.reminder.reminder_get_reminders,
+        nanapi.user.user_get_profile,
+        nanapi.waicolle.waicolle_get_players,
+        nanapi.waicolle.waicolle_get_player,
+        nanapi.waicolle.waicolle_get_player_tracked_items,
+        nanapi.waicolle.waicolle_get_player_track_unlocked,
+        nanapi.waicolle.waicolle_get_player_track_reversed,
+        nanapi.waicolle.waicolle_get_player_media_stats,
+        nanapi.waicolle.waicolle_get_player_staff_stats,
+        nanapi.waicolle.waicolle_get_player_collection_stats,
+        nanapi.waicolle.waicolle_get_waifus,
+        nanapi.waicolle.waicolle_trade_index,
+        nanapi.waicolle.waicolle_get_collection,
+    ]
+    for endpoint in endpoints:
+        yield Tool(endpoint)
