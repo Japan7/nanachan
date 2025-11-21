36ab4c2874453876e1e80ab66bf5f3cc69707dd3
NextFire <git@yuru.moe>
2025-11-21 13:32:40 +0100

    ai: chat on mention

--- nanachan/extensions/ai.py
+++ nanachan/extensions/ai.py
@@ -120,8 +120,7 @@ If the agent is asked to factcheck something, this something may be a replied me
         embed.set_footer(text=model_name)
         resp = await ctx.reply(embed=embed)
 
-        thread = await self.get_chat_thread(resp, name='/ai chat')
-        await thread.add_user(ctx.author)
+        thread = await self.get_chat_thread(resp, name='/ai chat', user_to_add=ctx.author)
         self.contexts[thread.id] = ChatContext(model_name=model_name)
 
         assert self.bot.user
@@ -130,7 +129,12 @@ If the agent is asked to factcheck something, this something may be a replied me
             allowed_mentions=AllowedMentions.none(),
         )
 
-    async def get_chat_thread(self, message: discord.Message, name: str):
+    async def get_chat_thread(
+        self,
+        message: discord.Message,
+        name: str,
+        user_to_add: discord.Member | discord.User | None = None,
+    ):
         if isinstance(message.channel, discord.Thread):
             thread = message.channel
         else:
@@ -138,6 +142,8 @@ If the agent is asked to factcheck something, this something may be a replied me
                 name=f'{name} – {datetime.now(TZ):%Y-%m-%d %H:%M}',
                 auto_archive_duration=60,
             )
+        if user_to_add:
+            await thread.add_user(user_to_add)
         return thread
 
     async def chat(
@@ -242,8 +248,7 @@ If the agent is asked to factcheck something, this something may be a replied me
         embed.set_footer(text=model_name)
         resp = await ctx.reply(embed=embed)
 
-        thread = await self.get_chat_thread(resp, name=prompt.name)
-        await thread.add_user(ctx.author)
+        thread = await self.get_chat_thread(resp, name=prompt.name, user_to_add=ctx.author)
         self.contexts[thread.id] = ChatContext(model_name=model_name)
 
         await self.chat(ctx, thread, chat_prompt, [])
@@ -253,32 +258,27 @@ If the agent is asked to factcheck something, this something may be a replied me
         if message.author.bot:
             return
 
-        if (
-            self.bot.user in message.mentions
-            and isinstance(message.channel, discord.Thread)
-            and message.channel.id in self.contexts
-        ):
-            ctx = await self.bot.get_context(message, cls=commands.Context[Bot])
-            await self.chat(
-                ctx,
-                message.channel,
-                message.content,
-                message.attachments,
+        if self.bot.user in message.mentions:
+            await self.on_chat_message(
+                message,
+                thread_name='/ai chat',
+                model_name=AI_DEFAULT_MODEL,
             )
             return
 
         if message.content.startswith('@grok'):
-            ctx = await self.bot.get_context(message, cls=commands.Context[Bot])
-            thread = await self.get_chat_thread(message, name='@grok')
-            await thread.add_user(message.author)
-            self.contexts[thread.id] = ChatContext(model_name=AI_GROK_MODEL)
-            await self.chat(
-                ctx,
-                thread,
-                message.content.removeprefix('@grok '),
-                message.attachments,
+            await self.on_chat_message(
+                message,
+                thread_name='@grok',
+                model_name=AI_GROK_MODEL,
             )
             return
+
+    async def on_chat_message(self, message: discord.Message, thread_name: str, model_name: str):
+        ctx = await self.bot.get_context(message, cls=commands.Context[Bot])
+        thread = await self.get_chat_thread(message, name=thread_name, user_to_add=message.author)
+        self.contexts[thread.id] = ChatContext(model_name)
+        await self.chat(ctx, thread, message.content, message.attachments)
 
 
 async def setup(bot: Bot):
--- nanachan/extensions/tsundere.py
+++ nanachan/extensions/tsundere.py
@@ -1,51 +1,15 @@
 import random
-from datetime import datetime
 
-from discord import AllowedMentions
 from discord.ext import commands
 from discord.ext.commands import Bot
-from pydantic_ai import Agent, RunContext
 
 from nanachan.discord.helpers import MultiplexingContext
-from nanachan.extensions.ai import AI
-from nanachan.settings import AI_LOW_LATENCY_MODEL, TZ, RequiresAI
-from nanachan.utils.ai import get_model
+from nanachan.settings import RequiresAI
 
 
 class Tsundere(commands.Cog):
-    messages = [
-        '何言ってんの？ばか！ 😡',
-        '別にそんなことないけど…',
-        'へ？どういう意味？アホか？',
-        'とっとと消え失せろ',
-    ]
-
-    @staticmethod
-    def system_prompt(run_ctx: RunContext[MultiplexingContext]):
-        ctx = run_ctx.deps
-        assert ctx.bot.user
-        return f"""
-The assistant is {ctx.bot.user.display_name}, a Discord bot.
-
-The current date is {datetime.now(TZ)}.
-
-{ctx.bot.user.display_name} responds in short sentences in Japanese, only using Japanese characters, that sound tsundere.
-{ctx.bot.user.display_name} avoids including 別に in its response.
-"""  # noqa: E501
-
-    @staticmethod
-    def author_instructions(run_ctx: RunContext[MultiplexingContext]):
-        ctx = run_ctx.deps
-        assert ctx.bot.user
-        return (
-            f'{ctx.bot.user.display_name} is now being connected with {ctx.author.display_name}.'
-        )
-
     def __init__(self, bot):
         self.bot = bot
-        self.agent = Agent(deps_type=MultiplexingContext)
-        self.agent.system_prompt(self.system_prompt)
-        self.agent.instructions(self.author_instructions)
 
     @commands.Cog.listener()
     async def on_user_message(self, ctx: MultiplexingContext):
@@ -54,22 +18,15 @@ The current date is {datetime.now(TZ)}.
             return
 
         if self.bot.user.display_name in message.content or self.bot.user in message.mentions:
-            ai_cog = AI.get_cog(self.bot)
-            if ai_cog is not None and ctx.channel.id in ai_cog.contexts:
-                return
-
-            if RequiresAI.configured:
-                run = await self.agent.run(
-                    message.clean_content,
-                    model=get_model(AI_LOW_LATENCY_MODEL),
-                    deps=ctx,
-                )
-                content = run.output
-            else:
-                content = random.choice(self.messages)
-
-            await message.reply(content, allowed_mentions=AllowedMentions.none())
+            messages = [
+                '何言ってんの？ばか！ 😡',
+                '別にそんなことないけど…',
+                'へ？どういう意味？アホか？',
+                'とっとと消え失せろ',
+            ]
+            await message.send(random.choice(messages))
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(Tsundere(bot))
+    if not RequiresAI.configured:
+        await bot.add_cog(Tsundere(bot))
